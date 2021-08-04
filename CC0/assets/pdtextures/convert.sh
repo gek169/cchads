2189ae091d2803affe3b8407d96e86df2eda132a
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-04 16:22:17 -0400

    Automatic commit.

--- CC0/assets/pdtextures/convert.sh
+++ CC0/assets/pdtextures/convert.sh
@@ -4,6 +4,6 @@ for filename in $JPGFILE
 do
  echo "Found file $filename"
  convert $filename -resize 512x512 512x512/$filename
- #convert $filename -resize 1024x1024 1024x1024/$filename
+ convert $filename -resize 1024x1024 1024x1024/$filename
  convert $filename -resize 256x256 256x256/$filename
 done
